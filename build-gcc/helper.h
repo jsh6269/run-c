4db1e6c4651552a63ab9381642907fef071017ea
suhan8984 <suhan8984@daum.net>
2025-08-04 22:40:47 +0900

    preset: HW2

--- build-gcc/helper.h
+++
@@ -1,9 +0,0 @@
-#ifndef HELPER_H
-#define HELPER_H
-
-class Helper {
-public:
-    void greet();
-};
-
-#endif
--- build-make/helper.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "helper.h"
-
-void Helper::greet() {
-    std::cout << "Hello from Helper class!" << std::endl;
-}
